19228e1a2ae865cb1d4c300fc678ffbb3f05737f
Ruifeng <rzhan016@ucr.edu>
2018-10-24 18:31:00 -0700

    Added sign up screen UI

---
+++ src/app/src/main/java/com/cs180/simplenote/simplenoteapp/SignUpActivity.java
@@ -0,0 +1,13 @@
+package com.cs180.simplenote.simplenoteapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
