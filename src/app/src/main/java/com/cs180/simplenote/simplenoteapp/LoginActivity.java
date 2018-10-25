7b4a21267acc3e8873941d831433a936a30d5102
Ruifeng <rzhan016@ucr.edu>
2018-10-24 18:43:28 -0700

    Added sign up button to login

--- src/app/src/main/java/com/cs180/simplenote/simplenoteapp/LoginActivity.java
+++ src/app/src/main/java/com/cs180/simplenote/simplenoteapp/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.cs180.simplenote.simplenoteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button SignUp = findViewById(R.id.sign_up_button);
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+
     }
 }
