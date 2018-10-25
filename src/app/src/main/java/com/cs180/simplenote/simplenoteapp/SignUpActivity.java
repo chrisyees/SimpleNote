276d2dbce78d1b54b443dae79e8e9c21e783c283
Ruifeng Zhang <rzhan016@ucr.edu>
2018-10-25 16:02:52 -0700

    Added firebase auth

--- src/app/src/main/java/com/cs180/simplenote/simplenoteapp/SignUpActivity.java
+++ src/app/src/main/java/com/cs180/simplenote/simplenoteapp/SignUpActivity.java
@@ -1,13 +1,46 @@
 package com.cs180.simplenote.simplenoteapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        Button createAccountButton = findViewById(R.id.create_account_button);
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"signing up", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
 }
+
