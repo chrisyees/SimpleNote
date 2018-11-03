57667e0303b6eb0abb216dec6a10fa68d3b2e536
Chris Yee <cyee008@ucr.edu>
2018-11-02 17:55:36 -0700

    adding initial label work to firebase, added some log outputs for firebase

--- src/app/src/main/java/com/cs180/simplenote/simplenoteapp/LabelsFragment.java
+++ src/app/src/main/java/com/cs180/simplenote/simplenoteapp/LabelsFragment.java
@@ -4,14 +4,49 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LabelsFragment extends Fragment {
+
+    private EditText addLabel;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_labels, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_labels, container, false);
+
+        addLabel = view.findViewById(R.id.add_label);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button add_label_button = (Button) view.findViewById(R.id.add_label_button);
+
+        add_label_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String noteText = addLabel.getText().toString(); //get user input
+
+                Log.d("Firebase", "Beginning to write label.");
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("labels");
+                myRef.push().setValue(noteText); //push value to database
+
+                Log.d("Firebase", "Ending write to label.");
+            }
+        });
     }
 }
--- src/app/src/main/java/com/cs180/simplenote/simplenoteapp/NotesFragment.java
+++ src/app/src/main/java/com/cs180/simplenote/simplenoteapp/NotesFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -59,9 +60,13 @@ public class NotesFragment extends Fragment {
 
                 noteList.setText(notesList);
 
+                Log.d("Firebase", "Beginning to write note.");
+
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("test1W");
+                DatabaseReference myRef = database.getReference("Notes");
                 myRef.push().setValue(noteText); //push value to database
+
+                Log.d("Firebase", "Ending write to note.");
 
                 /*
                 TODO : ADD ABILITY TO DYNAMICALLY ADD BUTTONS(NOTES) TO SCROLL VIEW
